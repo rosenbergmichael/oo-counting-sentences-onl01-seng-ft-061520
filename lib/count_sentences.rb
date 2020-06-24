7e8883e6ecaaa2966b51fa3258eedbc15d0cad2f
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-24 16:28:49 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,27 @@ require 'pry'
 
 class String
 
+
   def sentence?
-    
+    self.end_with?(".")
   end
+
 
   def question?
-
+    self.end_with?("?")
   end
+
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+ 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if do |s|
+      s.length < 2 
+      end.count 
   end
+
+
 end
