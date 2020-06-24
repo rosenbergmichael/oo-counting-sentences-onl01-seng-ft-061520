ca73423c47b99f1b05faa1f9aa6e246f58b73264
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-24 14:48:16 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,9 +19,8 @@ class String
 
  
   def count_sentences
-      self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
-    #self.delete(" ")
-    #self.split(/\.|\?|\!/).count
+    self.delete(" ")
+    self.split(/\.|\?|\!/).count
   end
 
 
